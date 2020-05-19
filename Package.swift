3cf3fbe224fb9f8c441a8ccb5efaddb68a67455a
Ralf Ebert <info@ralfebert.de>
2020-05-19 19:25:28 +0200

    fix ios dependency

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MiniCache",
-    platforms: [.iOS(.v10), .macOS(.v10_13)],
+    platforms: [.iOS(.v11), .macOS(.v10_13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
